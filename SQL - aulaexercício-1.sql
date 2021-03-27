13d164f1f788e71786213015b014de731825dedc
josouzz <jo.souza@usp.br>
2021-03-27 15:20:24 -0300

    Adicionei Meu primeiro exercício de SQL

---
+++ SQL - aulaexercício-1.sql
@@ -0,0 +1,69 @@
+-- Criar uma base de dados de Clientes
+
+create database cadastros
+default character set utf8
+default collate utf8_general_ci;
+-- Criei a base de dados nomeada de cadastros e defini para caractéres brasileiros
+
+use cadastros;
+create table pessoas(
+    id int not null auto_increment,
+    nome varchar(30) not null,
+    nascimento date,
+    sexo enum('M','F'),
+    peso decimal(5,2),
+    altura decimal(3,2),
+    nacionalidade  varchar(15) default 'Brasil',
+    primary key (id)
+) default charset = utf8
+/* Antes de criar o table, é preciso selecionar a database, por isso o "use cadastros"
+   Dados que decidi coletar: nome, nascimento, sexo, peso, altura e nacionalidade
+*/
+
+desc pessoas;
+-- Esse comando me mostra as colunas da minha base de dados e quais são os tipos primitivos que atribuí
+
+
+insert into pessoas values
+(default, 'Pedro', '1997-02-31', 'M', '69', '1.65', 'Argentina'),
+(default, 'Angeline', '1989-11-11', 'F', '55', '1.7', 'França'),
+(default,'Carolina', '1970-01-08', 'F', '63.23', '1,65', default),
+(default, 'Jorge', '1987-12-31', 'M', '89', '1.85', 'Argentina'),
+(default, 'Angelica', '1970-05-11', 'F', '55', '1.6', 'EUA'),
+(default,'Carol', '1970-01-08', 'F', '63.23', '1,75', default),
+(default, 'Carlos', '1997-02-31', 'M', '79', '1.65', 'Argentina'),
+(default, 'Angelo', '1999-11-11', 'M', '100', '2', 'Italia'),
+(default,'Rosana', '1977-07-08', 'F', '63.23', '1,65', default),
+(default, 'Iolanda', '1987-045-24', 'F', '69', '1.65', 'Brasil'),
+(default, 'José', '1987-11-11', 'F', '55', '1.7', 'Portugal'),
+(default,'Igor', '1975-01-28', 'F', '63.23', '1.75', default),
+(default, 'João', '1996-10-16', 'M', '130.00', '1.8', default),
+(default, 'Leticia', '1998-10-02', 'F', '65.3', '1.69', default),
+(default, 'Itamar', '1973-07-09', 'M', '85.6', '1.73', default);
+/*
+Insert into é o comando para fazer os inputs na base de dados */
+
+select * from pessoas;
+-- comando para vizualizar os dados da database
+
+alter table pessoas 
+add column profissão varchar (10);
+--Comando para alterar a tabela, aqui eu adicionei uma coluna
+
+alter table pessoas
+drop column profissão;
+-- Nesse alter table eu apaguei a coluna antes adicionada
+
+alter table pessoas
+add column profissao varchar(15) after nome;
+-- Readicionei a coluna profissão e a posicionei após a coluna nome
+
+alter table pessoas 
+modify profissao varchar (20) not null default ' ';
+--Modifiquei o valor da constraint varchar para 25 e defini que não podia receber valores nulos
+
+alter table pessoas 
+change column profissao profissão varchar (25);
+--Minha última alteração foi usando o comando change para alterar o nome da coluna
+
+select * from pessoas;
