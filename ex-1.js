2244e79f76c6888512d5bb95924733d0d9c17e7f
josouzz <jo.souza@usp.br>
2021-03-29 20:26:29 -0300

    exercício 1

---
+++ ex-1.js
@@ -0,0 +1,46 @@
+/*
+1) Cite as diferenças entre let e const
+2) Declare variáveis que tenham relação com a vida de vocês ou a natureza:
+  a) 5 strings
+  b) 5 booleanos
+  c) 5 números
+
+Durante a declaração, leve em conta se é um valor constante ou não. Use a declaração let e const de acordo.
+*/
+
+/*
+1) Const é usado para declaração de constantes, ou seja, valores que não se alteram. Já o o comando "let" serve
+para variáveis locais que podem ter seus valores alterados. 
+ *
+
+/*
+ 2a) 5 strings
+*/ 
+
+let lema = "O pai ta on";
+let ragnar = "Odin deu um olho em troca de conhecimento, eu daria muito mais";
+let cancer = "melhor signo";
+let nome = "João Pedro da Silva Souza";
+let azeite = "Extra virgem"
+
+/*
+ 2b) 5 booleans
+  */
+
+let paiTaTriste = true;
+let deusEHpai = true;
+let tudoPossoNaquiloQMeFortalece = true;
+let felicidade = false;
+let sol = false;
+
+
+/*
+2c) 5 numbers 
+*/
+
+const PI = 3.14;
+const SQTR2 = 1.414;
+let idade = 24;
+let altura = 180;
+let força = 8000.1;
+
