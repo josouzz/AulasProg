8e881f4546d282d3a3685cd109f106f71dea4638
josouzz <jo.souza@usp.br>
2021-03-30 22:31:21 -0300

    py

---
+++ Estudos de PY/Exercícios/Operações-básicas.py
@@ -0,0 +1,21 @@
+# Operações básicas com  dois números
+
+n1 = float(input("digite um valor: "))
+n2 = float(input("digite outro valor: "))
+
+s = n1 + n2
+# S de soma
+
+sub = n1 - n2
+# Sub de subtração
+
+m = n1 * n2
+# M de multiplicação
+
+d = n1 / n2
+# D de divisão
+
+e = n1 ** n2
+# E de exponenciação
+
+print(f'A soma é: {s}, a subtração é: {sub}, a multiplicação é: {m}, a dibisão é: {d} e a exponenciação é: {e}')
