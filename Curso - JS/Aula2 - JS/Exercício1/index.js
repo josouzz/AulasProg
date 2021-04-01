4bc8df0517af7ef6702bcabb77c0ff86ab052cf9
josouzz <jo.souza@usp.br>
2021-04-01 11:01:01 -0300

    Exercícios da aula 2

--- Curso - JS/Aula2 - JS/Exercício1/index.js
+++ Curso - JS/Aula2 - JS/Exercício1/index.js
@@ -3,11 +3,19 @@
  */
 
 
- let a = true || 0 && -1 && false;
+ let a = true || 0 && -1 && false; 
  let b = 50 && -1 || true && -1 || false;
  let c = 0 || 0 && true || false;
  let d = 10 || 20 && "texto" && -1;
  
+
+ /**
+  * Resultados do exercício 1, antes de rodar o node.js:
+  * A) FALSE
+  * B)TRUE
+  * C)TRUE
+  * D)TRUE
+  */
  
  /**
   * Determinem, via código, os resultados da seguinte equação de segundo grau:
@@ -17,3 +25,26 @@
   * Caso a equação não possa ser resolvida, imprima na tela o motivo.
   */
  
+ a = 2
+ 
+ b = 4
+ 
+ c = -5
+ 
+ delta = Math.pow(b, 2) - (4*a*c)
+ 
+ d = Math.sqrt(delta)
+
+ x1 = (-b + d)/(2 * a)
+
+ x2 = (-b - d)/(2 * a)
+
+ if (d > 0) {
+     console.log("As raízes dessa equação são:", x1, "e", x2);
+
+ } else if (d === 0) {
+     console.log("A raiz dessa equação é:", x1);
+
+ } else {
+     console.log("Essa equação não possuí raiz no conjuto dos REAIS");
+ }
